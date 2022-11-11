35c1e26b8e162eb7012cf07978d8c1d013951538
lukekeum <lukekeum11@gmail.com>
2022-11-11 22:48:22 +0900

    fix: change request body

--- client/src/hooks/useAuthUser.ts
+++ client/src/hooks/useAuthUser.ts
@@ -25,76 +25,68 @@ export default function useAuthUser() {
 
   const loginUser = useCallback(
     async ({ email, username, password }: LoginUserInput) => {
-      try {
-        if (!email && !username) {
-          toast.error('아이디나 이메일을 제대로 입력해주세요');
-        }
-
-        const response = await Client.post('/samshisaeki/auth/login', {
-          email,
-          username,
-          password,
-        });
-
-        if (response.status !== 201) {
-          toast.error(response.data.msg);
-          return;
-        }
-
-        setAuthToken(response.data.token);
-
-        toast.info('성공적으로 로그인했습니다');
-
-        setModal({
-          isOpened: false,
-          type: 'login',
-        });
-
-        const user = response.data.user as { name: string };
-
-        setUser({
-          username: user.name,
-          nickname: user.name,
-        });
-      } catch (err) {
-        toast.error('로그인 중 오류가 발생했습니다');
+      if (!email && !username) {
+        toast.error('아이디나 이메일을 제대로 입력해주세요');
       }
+
+      const response = await Client.post('/samshisaeki/auth/login', {
+        email,
+        id: username,
+        password,
+      });
+
+      if (response.status !== 201) {
+        toast.error(response.data.msg);
+        return;
+      }
+
+      setAuthToken(response.data.token);
+
+      toast.info('성공적으로 로그인했습니다');
+
+      setModal({
+        isOpened: false,
+        type: 'login',
+      });
+
+      const user = response.data.user as { name: string };
+
+      setUser({
+        username: user.name,
+        nickname: user.name,
+      });
     },
     [setAuthToken, setModal, setUser]
   );
 
   const registerUser = useCallback(
     async ({ email, username, password }: RegisterInput) => {
-      try {
-        const response = await Client.post('/samshisaeki/auth/register', {
-          email,
-          username,
-          password,
-        });
+      const response = await Client.post('/samshisaeki/auth/register', {
+        email,
+        id: username,
+        password,
+      });
 
-        if (response.status !== 201) {
-          toast.error(response.data.msg);
-          return;
-        }
-
-        setAuthToken(response.data.token);
-
-        toast.info('성공적으로 회원가입했습니다');
-
-        setModal({
-          isOpened: false,
-          type: 'register',
-        });
-
-        const user = response.data.user as { name: string };
-
-        setUser({
-          username: user.name,
-          nickname: user.name,
-        });
-      } catch (err) {
-        toast.error('회원가입 중 오류가 발생했습니다');
+      if (response.status !== 201) {
+        toast.error(response.data.msg);
+        return;
       }
+
+      setAuthToken(response.data.token);
+
+      toast.info('성공적으로 회원가입했습니다');
+
+      setModal({
+        isOpened: false,
+        type: 'register',
+      });
+
+      const user = response.data.user as { name: string };
+
+      setUser({
+        username: user.name,
+        nickname: user.name,
+      });
     },
     [setAuthToken, setModal, setUser]
   );
