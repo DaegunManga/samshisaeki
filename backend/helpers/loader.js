c559c46fd0418e1f1349cdaa59b63531e6839dfc
lukekeum <lukekeum11@gmail.com>
2023-03-13 17:38:38 +0900

    fix: change python spawn to axios
    [Critcal] Buffer 길이 초과 문제 #10

--- backend/helpers/loader.js
+++ backend/helpers/loader.js
@@ -1,28 +1,22 @@
 // 파이썬으로 크롤링한 정보 받아오는 스크립트
 
-const { spawn } = require("child_process");
-const iconv = require("iconv");
+const { spawn } = require('child_process');
+const iconv = require('iconv');
+const axios = require('axios');
 
 const fetchMeals = (req, res, next) => {
   return new Promise((resolve, reject) => {
     try {
-      var dataToSend;
-      var ic = new iconv.Iconv("euc-kr", "UTF-8");
-      const runCommand = spawn("python", ["scrap.py"]);
+      axios.get(`${process.env.MEAL_SERVER_ADDRESS}`).then((response) => {
+        req.body = response.data;
 
-      runCommand.stdout.on("data", (data) => {
-        dataToSend = JSON.parse(data.toString());
-      });
-
-      runCommand.on("close", (err) => {
-	req.body = dataToSend;
-	next();
-        resolve(dataToSend);
+        next();
+        resolve(response.data);
       });
     } catch (err) {
       reject(err);
     }
-  })
+  });
 };
 
 module.exports = {
